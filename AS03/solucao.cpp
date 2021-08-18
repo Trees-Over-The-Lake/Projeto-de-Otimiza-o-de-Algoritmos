277eccfe4a66ec0860d65a6d3a55085fb44c77d3
Lucas Santiago <lu.santi.oli@gmail.com>
2021-08-17 21:44:38 -0300

    feat: AS03 resultado 100%

--- AS03/solucao.cpp
+++ AS03/solucao.cpp
@@ -2,25 +2,45 @@
 #include <string>
 #include <iostream>
 
+bool existeSubstring(std::string string, std::string substring) {
+    uint16_t posEncontrado = 0;
+
+    for(char c : string) {
+        if(c == substring.at(posEncontrado))
+            posEncontrado++;
+
+        // Se deslocar corretamente toda a substring e chegar até o final
+        // significa que toda ele foi encontrada na string original
+        if(posEncontrado == substring.length())
+            return true;
+    }
+
+    return false;
+}
+
 // Solucao para o problema de subsequencias
 int main(int argc, char **argv) {
     uint16_t casosTeste;
     std::cin >> casosTeste;
     std::cin.ignore();
 
-    std::string casos[casosTeste];
-    for(uint16_t i = 0; i < casosTeste; i++) 
-        std::cin >> casos[i];
-    
+    std::string caso;
+    for(uint16_t i = 0; i < casosTeste; i++) {
+        std::cin >> caso; 
 
-    uint16_t numSubsequencias;
-    std::cin >> numSubsequencias;
-    std::cin.ignore();
+        uint16_t queries; 
+        std::cin >> queries; 
+        std::cin.ignore();
 
-    std::string subsequencias[numSubsequencias];
-    for(uint16_t i = 0; i < numSubsequencias; i++) 
-        std::cin >> subsequencias[i];
-    
-    
+        std::string subsequencia;
+        for(uint16_t j = 0; j < queries; j++) {
+            std::cin >> subsequencia; 
+
+            if(existeSubstring(caso, subsequencia))
+                std::cout << "Yes" << std::endl;
+            else
+                std::cout << "No" << std::endl;
+        }
+    }
     return 0;
 }
