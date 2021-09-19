b5900d6c6049cf0745b4a5a185ee224537b912ac
Lucas Santiago <lu.santi.oli@gmail.com>
2021-09-19 15:29:08 -0300

    feat: AS05 finished

--- AS05/solucao.cpp
+++ AS05/solucao.cpp
@@ -1,6 +1,12 @@
-// Solution for uri judge 1082 -> https://www.urionlinejudge.com.br/judge/pt/problems/view/1082
+/*
+    Solução para o urijudge 1082 -> https://www.urionlinejudge.com.br/judge/pt/problems/view/1082
+    Professor: Wladmir Cardoso Brandão
+    Matéria: Laboratório de Projeto de Algoritmos
+    Aluno: Lucas Santiago de Oliveira
+    Matrícula: 650888
+    Complexidade do algoritmo: O(V^2) no pior caso, deslocando duas vezes  
+*/
 
-//AS04: Gustavo Lopes
 #include <vector>
 #include <cstdint>
 #include <array> 
@@ -8,59 +14,39 @@
 #include <iostream>
 #include <algorithm>
 
-/* Complexidade: O(V^2), pois o algoritmo precisa atravessar todas as
-*  vertices(V) e no pior caso, durante o DFS, ele vai precisar atravessar
-*  o grafo inteiro, logo, tornando o algoritmo mais custoso
-*/
-
 // Classe de grafos
 class Graph {
 public:
-
-    // Num de vertices e arestas
-    uint32_t vertices;
-    uint32_t edges;
+    uint32_t numVertices;
+    uint32_t numEdges;
 
     // Lista de adjacencia 
     std::vector<std::vector<uint32_t>> adj;
 
-
     // Iniciar grafo com 'v' numero de vertices
     Graph(uint32_t v) {    
-        this->edges    = 0;
-        this->vertices = 0;
-
-        for(int i =0 ; i < v; i++) 
-            add_vertex();
-    }
-
-    // Retornar numero de vertices
-    uint32_t vertices_num() {
-        return this->vertices;
+        this->numEdges = this->numVertices = 0;
+        for(int i =0 ; i < v; i++) addVertex();
     }
 
     // Adicionar vertice na lista de adjacencia
-    void add_adj(uint32_t first, uint32_t last) {
-        auto it = &this->adj.at(first); // Resgatar ponteiro para a lista na posicao 'first'
-
-        // Inserir 'last' para a lista resgatada, como a variavel eh um 
-            // ponteiro, o elemento sera atualizado na memoria
+    void addAdj(uint32_t first, uint32_t last) {
+        std::vector<uint32_t> *it = &this->adj.at(first);
         it->push_back(last);            
     }
 
     // Adicionar aresta ao grafo
-    bool add_edge(uint32_t first, uint32_t last) {
+    bool addEdge(uint32_t first, uint32_t last) {
         bool result = false; // variavel de resultado da operacao
 
         //Se algum dos vertices nao existir no grafo
-        if(first < this->vertices_num() && last < this->vertices_num()) {
-            
+        if(first < this->numVertices && last < this->numVertices) {
             // Adicionar ao contador 
-            this->edges++;
+            this->numEdges++;
             
             // Adicionar vertices a lista de adjacencia
-            add_adj(first, last);
-            add_adj(last, first);
+            this->addAdj(first, last);
+            this->addAdj(last, first);
 
             result = true;
         }
@@ -69,123 +55,95 @@ public:
     }
 
     // Adicionar nova vertice
-    void add_vertex() {
-
-        this->vertices++;
-
-        //Adicionar nova lista de adjacencia
+    void addVertex() {
+        this->numVertices++;
         std::vector<uint32_t> it;
         this->adj.push_back(it);
     }
 
-    // Funcoes para contar numero de componentes
-    int num_components() {
+    int numberOfComponents() {
+        int num = 0;
 
-        int num = 0; // variavel de resultado
-
-        // Variavel para marcar vertices visitadas
-        bool* visited = new bool[this->vertices];
-        std::memset(visited, false,this->vertices*sizeof(bool)); // iniciando array com valor 'false'
+        // Iniciando uma variavel para marcar vertices visitadas como falso
+        bool* visited = new bool[this->numVertices];
+        for(int i = 0; i < this->numVertices; i++)
+            visited[i] = false;
     
         //Visitar todas as vertices
-        for (int v = 0; v < this->vertices; v++) {
-            if (visited[v] == false) {
+        for (int v = 0; v < this->numVertices; v++) {
+            if (!visited[v]) {
+                // Vetor contendo todos os vertexes já passados pelo dfs
+                std::vector<char> vertexes = this->dfs(v, visited);
 
-                // Fazer um 'depth first search', retornando vertices atravessados
-                auto vertexes = dfs(v, visited);
-
-                // Ordenar vertices e imprimir na tela
+                // Ordenar os vértices resultantes do dfs
                 std::sort(vertexes.begin(), vertexes.end());
                 for(int j = 0; j < vertexes.size(); j++) 
                     std::cout << vertexes.at(j) << ",";
                 
-                // aumentar numero de componentes
                 num++;
-                std::cout << "\n"; // dar espacamento
+                std::cout << std::endl; // dar espacamento
             }
         }
 
-        // deletar array da memoria
+        // Limpar os arranjos da memória
         delete visited;
-
         return num;
     }
 
-    // Funcao de 'depth first search'
-    std::vector<char> dfs(int v, bool* visited) {
-
+    // Depth First Search
+    std::vector<char> dfs(int vert, bool *visited) {
         // Marcar vertice como visitado
-        visited[v] = true;
+        visited[vert] = true;
+        std::vector<char> saida;
+        
+        // Inserindo o primeiro elemento no vetor ele já está sendo visitado
+        saida.push_back((char) vert); 
 
-        // Criar array de char, iniciando com a vertice
-        // que esta sendo visitada
-        std::vector<char> resp = { intToChar(v) };
-
-        // Pegar vizinhos de 'v'
-        auto curr_vertex_adj = this->adj.at(v); 
-
-        // Atravesar os vizinhos de 'v'
-        for (int i = 0 ; i < curr_vertex_adj.size(); i++)
-
-            // Se o vizinho de 'v' nao tiver sido visitado
-            // visita-lo
-            if (!visited[curr_vertex_adj.at(i)]) {
-
-                // Pegar vizinhos visitados e armazena-los no resultaod
-                auto tmp = dfs(curr_vertex_adj.at(i), visited);
-                for(int j = 0; j < tmp.size(); j++) {
-                    char aux = tmp.at(j);
-                    resp.push_back(aux);
-                }
+        // Deslocar os vizinhos
+        std::vector<uint32_t> adjVertex = this->adj.at(vert); 
+        for (int i = 0 ; i < adjVertex.size(); i++)
+            // Visitar os visinhos do vértice
+            if (!visited[adjVertex.at(i)]) {
+                // Salvar vizinhos visitados
+                auto tmp = dfs(adjVertex.at(i), visited);
+                for(int j = 0; j < tmp.size(); j++) 
+                    saida.push_back(tmp.at(j));
             }
 
-        return resp;
-    }
-
-    // Converter inteiro para caracetere
-    char intToChar(int i) {
-        return (char)(i + 97); // 97 = 'a' 
+        return saida;
     }
 };      
 
-// Funcao utilitaria para conversao de 'char' para 'inteiro'
-int charToInt(char c);
-
-int main() {
-
-    // Numero de casos testes
+// Pegando as entradas e escrevendo a resolução do problema 1082 do urijudge
+int main(int argc, char **argv) {
     int n;
-
     std::cin >> n;
-
-    for(int i = 0 ; i < n; i++) {
-
-        // Numero de vertices e arestas
-        int v,e;
-
+    std::cin.ignore();
+    
+    // Varíaveis que serão usados no programa, declaradas aqui para serem sobreescritas durante execução
+    // sem precisarem serem reinstanciadas
+    int v,e;
+    char v1,v2;
+    for(int i = 1 ; i != n; i++) {
         // Criando grafo
         std::cin >> v >> e;
-        Graph* g = new Graph(v);
+        std::cin.ignore();
 
-        char v1,v2;
+        Graph* graph = new Graph(v);
         for(int j = 0 ; j < e; j++) {
-
             // Fazer leitura da aresta 'j' do grafo
             std::cin >> v1 >> v2;
+            std::cin.ignore();
 
-            // Adicionar vertice ao grafo
-            // convertendo o char para um numero inteiro
-            g->add_edge(v1 - 'a', v2 - 'a');
-            
+            // Adicionar vertice ao grafo como um int
+            graph->addEdge(v1 - 'a', v2 - 'a');
         }
 
-        // Imprimindo o caso atual
-        std::cout << "Case #" << i + 1 << ":" << std::endl;
-        int num = g->num_components();
+        // Imprimindo o resultado
+        std::cout << "Case #" << i << ":" << std::endl;
+        int num = graph->numberOfComponents();
         std::cout << num << " connected components\n" << std::endl;
 
-        // Deletar grafo da memoria
-        delete g;
-
+        delete graph;  // Limpar o grafo da memória
     }
 }
